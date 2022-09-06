7bb015941412dab3bab35155eea446d1157a667b
erik.berglund@gmail.com <erik.berglund@gmail.com>
2022-09-06 18:46:32 +0200

    Basic functionality

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from isoweek import Week
 
 class WeekNumber(MycroftSkill):
     def __init__(self):
@@ -7,7 +7,9 @@ class WeekNumber(MycroftSkill):
 
     @intent_file_handler('number.week.intent')
     def handle_number_week(self, message):
-        self.speak_dialog('number.week')
+        current_week = str(Week.thisweek().week)
+        data = {"week":current_week}
+        self.speak_dialog('number.week', data)
 
 
 def create_skill():
